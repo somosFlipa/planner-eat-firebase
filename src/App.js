7d9a903e49636b448d030e9d50fa8b3b60bbdcd9
anamad8 <stellam_anabel@hotmail.com>
2022-03-29 10:32:04 -0300

    routa home-welcome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Routes, Route} from "react-router-dom";
 import CookingRecipe from "./components/cookingRecipe";
+import Home from "./components/Home";
+import Welcome from "./components/Welcome"
 
 const App = () => {
 
 return(
 <>
-<h1>Recetas</h1>
-<CookingRecipe/>
+    <h1>Recetas</h1>
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Welcome" element={<Welcome />} />
+        <Route exact path="/CookingRecipe" element={<CookingRecipe/>} />
+    </Routes>
+
 </>
 )
 };
