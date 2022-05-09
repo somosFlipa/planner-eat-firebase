a5534fbbda02a7172723f35618b5df39c60734a9
ManuelMU <manumerino01@gmail.com>
2022-05-09 09:57:01 -0300

    Css Opinion Modal y fin

--- src/components/Fin/Fin.jsx
+++ src/components/Fin/Fin.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import logo from '../../assets/Logo 2021-1.png'
-
+import "./Fin.css"
+import fondofondo from "../../assets/Rectangle.png"
+    
 function Fin() {
     return (
-        <div>
-            <div>
+        <>
+        <div className='flex-fondo-fin'>
+            <div className='logo-fin'>
                 <img src={logo} alt="" />
             </div>
-            <div>
+            <div >
                 <h2>¡Muchas gracias!</h2>
                 <p>Ya te conocemos mucho mejor {":)"} </p>
-                <p>En breve vas a recibir en tu correo la información relevante, Lista de compras y las recetas completas para que puedas armar tu menú.</p>
+                <p>En breve vas a recibir en tu correo la información relevante, Lista de compras y las recetas <br/ >completas para que puedas armar tu menú.</p>
             </div>
-            
+           
+        </div> 
+        <div className='fondofruta'>
+            <img src={fondofondo} alt="" />
         </div>
+        </>
     )
 }
 
