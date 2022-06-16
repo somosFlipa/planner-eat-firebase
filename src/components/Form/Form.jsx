454136f20f20c58ea99db0c96ed0126552ea5849
anamad8 <stellam_anabel@hotmail.com>
2022-06-16 15:19:11 -0300

    validacion de formulario

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,48 +2,72 @@ import React, {useState, useContext} from 'react';
 
 import './Form.css';
 
-import { Link } from "react-router-dom";
-//import {useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 
 
 import {RepiceContext} from "../../Context/RecipeContext.jsx";
-import e from 'cors';
+
 
 const Form = () =>{
 
   const {addToInfo}= useContext(RepiceContext)
-  //const history = useNavigate()
+  const history = useNavigate()
 
-  const [state, setState] = useState({});
+  // const [state, setState] = useState({});
+  const [nombre, setNombre] = useState({});
+  const [email, setEmail] = useState({});
+
 
     function handleChange(e){
-        setState({
-            ...state,
+      setNombre({
+            ...nombre,
             [e.target.name] : e.target.value
         })
+        setEmail({
+          ...email,
+          [e.target.name] : e.target.value
+      })
         
     }
 
-    function guardar() {
-      
-      addToInfo(state);
-      //history('/Welcome')
+    
+
+    const login = (nombre,email) => {
+    
+    if ( Object.entries(email).length !== 0 && Object.entries(email).length !== 0) {
+      addToInfo(nombre,email);
+      history('/Welcome')
+      // console.log('valido')
+    }else console.log("coloca")
+    
     }
+
+
+    // function guardar(e) {
+    //   e.preventDefault();
+    //   addToInfo(state);
+    //   // history('/Welcome')
+    // }
 
   
   return (
     <div>
-        <form className='FormContainer' >
+        <form className='FormContainer' onSubmit={e =>{
+          e.preventDefault();
+          login(nombre, email)
           
-          <input type="text" name="user_name" onChange={(e) => handleChange(e)} placeholder='Nombre Y Apellido*' required/>
+        }} >
           
-          <input type="email" name="user_email" onChange={(e) => handleChange(e)} placeholder='Email*' required/>
+          <input type="text" name="user_name" onChange={(e) => handleChange(e)} placeholder='Nombre Y Apellido*' />
+          
+          <input type="email" name="user_email" onChange={(e) => handleChange(e)} placeholder='Email*' />
 
           <div className='btn-comenzar'>
 
-            <Link className="btn-next-comenzar" to={'/Welcome'} onClick={guardar}>Comenzar</Link>
+            {/* <Link className="btn-next-comenzar" to={'/Welcome'} onClick={guardar}>Comenzar</Link> */}
 
-            {/*<button className="btn-next-comenzar" onClick={guardar}><Link to={'/Welcome'} ></Link> Comenzar</button>*/}
+            <button className="btn-next-comenzar" >Comenzar</button>
 
           </div>
 
