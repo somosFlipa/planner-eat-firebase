5a4565254c8a06dd16c5b9f067a1415936a2b3bc
anamad8 <stellam_anabel@hotmail.com>
2022-07-07 10:01:53 -0300

    emailjs

--- src/components/Fin/Fin.jsx
+++ src/components/Fin/Fin.jsx
@@ -4,24 +4,41 @@ import "./Fin.css"
 import fondofondo from "../../assets/Rectangle.png"
 import axios from 'axios';
 import {RepiceContext} from "../../Context/RecipeContext.jsx";
+import emailjs from '@emailjs/browser';
     
 function Fin() {
 
     const { guardar, guardarMensaje, datos, comensales } = useContext(RepiceContext)
 
-    function sendEmail() {
-        console.log("nombre",datos,
-                    "recetas",guardar, 
-                    "mensaje",guardarMensaje,
-                    "comensales", comensales)
+    console.log("resetas",guardar)
+    console.log("mensaje",guardarMensaje)
+    console.log("nombre",datos)
+    console.log("comensales",comensales)
+    let data = [guardar, guardarMensaje, datos, comensales]
+
+    const sendEmail = () => {
+        // e.preventDefault();
+    
+        emailjs.sendForm('service_bc397fi', 'template_ivkegio', data, '7fxso87HjOOcbYriT')
+          .then(result => console.log(result.text))
+          .catch (error => console.log(error.text))
+      };
+
+    // function sendEmail() {
+    //     console.log("nombre",datos,
+    //                 "recetas",guardar, 
+    //                 "mensaje",guardarMensaje,
+    //                 "comensales", comensales)
         
-        let data = [guardar, guardarMensaje, datos, comensales]
-        const json = axios.post('http://localhost:8080/api/form',data)
-        .then(res=>{
-            console.log(res)
-        })
-        console.log(json)
-    }
+    //     let data = [guardar, guardarMensaje, datos, comensales]
+    //     const json = axios.post('http://localhost:8080/api/form',data)
+    //     .then(res=>{
+    //         console.log(res)
+    //     })
+    //     console.log(json)
+    // }
+
+
 
     return (
         <>
@@ -31,7 +48,7 @@ function Fin() {
                     <img src={logo} alt="" />
                 </div>
                 <div className='div-fin-h2-p-p'>
-                    {sendEmail()}
+                    {sendEmail}
                     <h2>¡Muchas gracias!</h2>
                     <p className='p-div-info-14'>Ya te conocemos mucho mejor {":)"} </p>
                     <p className='p-div-info-12'>En breve vas a recibir un correo por parte de nuestro equipo con la lista de compras, recetas e información extra de los platos que seleccionaste. Para que juntos armemos tu menú semanal.</p>
